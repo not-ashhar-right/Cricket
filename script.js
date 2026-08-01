5747c64fc19aeaabc309662f31b41475636336c7
not-ashhar-right <ashhar8925@gmail.com>
2026-08-02 01:17:51 +0530

    functions javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let comptchoice;
 function myFunction() {
+  let comptchoice;
   num = Math.ceil(Math.random() * 3);
   if (num == 1) {
     comptchoice = "ball";
@@ -8,4 +8,39 @@ function myFunction() {
   } else if (num == 3) {
     comptchoice = "stump";
   }
+  return comptchoice;
+}
+
+function compare(userchoice, comptchoice) {
+  if(userchoice==='bat'){
+    if (comptchoice == 'ball') {
+            return 'you have won the game';
+          } else if (comptchoice == 'bat') {
+            return 'draw game';
+          } else if (comptchoice == 'stump') {
+            return 'you have lost the game';
+          }
+        }
+  else if(userchoice==='ball'){
+    if (comptchoice == 'ball') {
+            return 'draw game';
+          } else if (comptchoice == 'bat') {
+            return 'you have lost the game';
+          } else if (comptchoice == 'stump') {
+            return 'you have won the game';
+          }
+        }
+  else if(userchoice==='stump'){
+    if (comptchoice == 'ball') {
+            return 'You have lost the game';
+          } else if (comptchoice == 'bat') {
+            return 'you have won game';
+          } else if (comptchoice == 'stump') {
+            return 'draw game';
+          }
+  }
+}
+
+function alerting(userchoice, comptchoice, result){
+  alert(`you choose ${userchoice},computer choose ${comptchoice} : ${result}`);
 }
