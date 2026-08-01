6be497db82906f03598df89c092a027f0b13934b
not-ashhar-right <ashhar8925@gmail.com>
2026-08-01 22:14:22 +0530

    adding JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+let comptchoice;
+function myFunction() {
+  num = Math.ceil(Math.random() * 3);
+  if (num == 1) {
+    comptchoice = "ball";
+  } else if (num == 2) {
+    comptchoice = "bat";
+  } else if (num == 3) {
+    comptchoice = "stump";
+  }
+}
