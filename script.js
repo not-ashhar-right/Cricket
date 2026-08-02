ea0b6714be66a2564eac02bbfb383a5e7a869ace
not-ashhar-right <ashhar8925@gmail.com>
2026-08-03 00:03:58 +0530

    storing the Values

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+let score={
+    win:0,
+    lose:0,
+    draw:0,
+    resultDisplay:function(){
+      return `the number of matches won:${score.win},lost:${score.lose},draw:${score.draw}`;
+    }
+  }
+
 function myFunction() {
+  
   let comptchoice;
   num = Math.ceil(Math.random() * 3);
   if (num == 1) {
@@ -14,33 +24,43 @@ function myFunction() {
 function compare(userchoice, comptchoice) {
   if(userchoice==='bat'){
     if (comptchoice == 'ball') {
+      score.win++;
             return 'you have won the game';
           } else if (comptchoice == 'bat') {
+            score.draw++;
             return 'draw game';
           } else if (comptchoice == 'stump') {
+            score.lose++;
             return 'you have lost the game';
           }
         }
   else if(userchoice==='ball'){
     if (comptchoice == 'ball') {
+      score.draw++;
             return 'draw game';
           } else if (comptchoice == 'bat') {
+            score.lose++;
             return 'you have lost the game';
           } else if (comptchoice == 'stump') {
+            score.win++;
             return 'you have won the game';
           }
         }
   else if(userchoice==='stump'){
     if (comptchoice == 'ball') {
+            score.lose++;
             return 'You have lost the game';
           } else if (comptchoice == 'bat') {
+            score.win++;
             return 'you have won game';
           } else if (comptchoice == 'stump') {
+            score.draw++;
             return 'draw game';
           }
   }
 }
 
 function alerting(userchoice, comptchoice, result){
-  alert(`you choose ${userchoice},computer choose ${comptchoice} : ${result}`);
+  alert(`you choose ${userchoice},computer choose ${comptchoice} : ${result} 
+    ${score.resultDisplay()}`);
 }
